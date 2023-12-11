3101dc6ecd3980a0b8c8172e359355e1aba89011
Linuszand <linus.zand@gmail.com>
2023-12-11 18:01:17 +0100

    added test to TicketCounter
    Added mockdata test that works

--- client/src/components/bookMovie/TicketCounter.test.jsx
+++ client/src/components/bookMovie/TicketCounter.test.jsx
@@ -3,20 +3,39 @@ import TicketCounter from "./TicketCounter";
 import renderer from "react-test-renderer";
 
 jest.mock('../../react-easier', () => ({
-  useStates: jest.fn(() => ({ adult: 0, child: 0, senior: 0, total: 0 })), // Adjust the mock state as needed
+  useStates: jest.fn(() => ({ adult: 0, child: 1, senior: 2})), // Sets value of the amount of people
 }));
 
+  describe('TicketCounter', () => {
+    test('renders with initial values', () => {
+      const setSeatsMock = jest.fn();
+      render(
+        <TicketCounter
+          screening={{}}
+          movie={{}}
+          seats={[]}
+          setSeats={setSeatsMock}
+        />
+      );
 
+      // Check if mockdata values are in the document
+      expect(screen.getByText('0')).toBeInTheDocument(); // adult
+      expect(screen.getByText('1')).toBeInTheDocument(); // child
+      expect(screen.getByText('2')).toBeInTheDocument(); // senior
+    });
 
-  test("snapshot testing", () => {
-    const setSeatsMock = jest.fn();
-    const snap = renderer.create(<TicketCounter
-        screening={{}}
-        movie={{}}
-        seats={[]}
-        setSeats={setSeatsMock}
-      />).toJSON();
-    expect(snap).toMatchSnapshot("");
+    test("snapshot testing", () => {
+      const setSeatsMock = jest.fn();
+      const snap = renderer.create(<TicketCounter
+          screening={{}}
+          movie={{}}
+          seats={[]}
+          setSeats={setSeatsMock}
+        />).toJSON();
+      expect(snap).toMatchSnapshot("");
+    });
   });
+
+  
 
 
